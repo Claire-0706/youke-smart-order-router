e23305a0820c1f49fcc98ccc440e0dd764f9c1a6
hq22 <hq@yw.zj.cn>
2025-06-03 08:36:42 +0800

    hello

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
+import express from "express";
+import cors from "cors";
+import quoteHandler from "./api/quote";
 
-import express from 'express'
-import cors from 'cors'
-import quoteHandler from './api/quote'
+const app = express();
+const port = process.env.PORT || 3000;
 
-const app = express()
-const port = process.env.PORT || 3000
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
+app.get("/", (_, res) => {
+  res.send("hello");
+});
 
-app.post('/api/quote', quoteHandler)
+app.post("/api/quote", quoteHandler);
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+  console.log(`Server listening on port ${port}`);
+});
